414a0c66f8e102e814ace52918d43ac9c54bf0c3
CristianCantoro <kikkocristian@gmail.com>
2016-08-23 23:02:34 +0200

    Temporary fix: add javascript to fix links in search results

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,13 @@
 $('#search_results').tapir({
   'token': '570e9e6314ad6644b943ab78'
 });
+
+// watch for creation of an element which satisfies the selector ".test-elem"
+$(document).arrive(".post-content a", function() {
+    // 'this' refers to the newly created element
+    // var $newElem = $(this);
+    $(this).each(function(){
+          var newUrl = this.href.replace('/feed.xml/', '');
+          $(this).attr('href', newUrl);
+        });
+});
